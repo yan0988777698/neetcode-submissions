a23127c52142f4aea8cb8be86c26499b2a3d8fc3
Yen-Lin Chen <90600356+yan0988777698@users.noreply.github.com>
2026-04-25 19:23:03 +0800

    Add: trapping-rain-water - submission-3

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-3.cs
@@ -0,0 +1,29 @@
+public class Solution {
+    public int Trap(int[] height) {
+        int left = 0;
+        int right = height.Length - 1;
+
+        int maxLeft = 0;
+        int maxRight = 0;
+
+        int water = 0;
+        while (left < right) {
+            if (height[left] < height[right]) {
+                if (height[left] >= maxLeft) {
+                    maxLeft = height[left];
+                } else {
+                    water += maxLeft - height[left];
+                }
+                left ++;
+            } else {
+                if (height[right] >= maxRight) {
+                    maxRight = height[right];
+                } else {
+                    water += maxRight - height[right];
+                }
+                right--;
+            }
+        }
+        return water;
+    }
+}
