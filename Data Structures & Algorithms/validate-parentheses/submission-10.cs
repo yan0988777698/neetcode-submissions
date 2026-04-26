8416ba13b07a3190b5ba7c757a0d59a5ac0e9c64
Yen-Lin Chen <90600356+yan0988777698@users.noreply.github.com>
2026-04-26 15:00:16 +0800

    Add: validate-parentheses - submission-10

---
+++ Data Structures & Algorithms/validate-parentheses/submission-10.cs
@@ -0,0 +1,30 @@
+public class Solution
+{
+    public bool IsValid(string s)
+    {
+        var stack = new Stack<char>();
+
+        foreach (char c in s)
+        {
+            if (c == '(')
+            {
+                stack.Push(')');
+            }
+            else if (c == '[')
+            {
+                stack.Push(']');
+            }
+            else if (c == '{')
+            {
+                stack.Push('}');
+            }
+            else
+            {
+                if (stack.Count == 0 || stack.Pop() != c)
+                    return false;
+            }
+        }
+
+        return stack.Count == 0;
+    }
+}
