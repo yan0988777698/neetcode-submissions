ecc0f6b4753e99abe10a0a141db76510ce9da051
Yen-Lin Chen <90600356+yan0988777698@users.noreply.github.com>
2026-07-11 17:54:46 +0800

    Add: car-fleet - submission-5

---
+++ Data Structures & Algorithms/car-fleet/submission-5.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public int CarFleet(int target, int[] position, int[] speed) {
+        var cars = position
+                       .Select((p, index) =>
+                                   new { Position = p, Time = (double)(target - p) / speed[index] })
+                       .OrderByDescending(car => car.Position)
+                       .ToList();
+
+        Stack<double> carFleet = new Stack<double>();
+
+        foreach (var car in cars) {
+            double time = car.Time;
+            if (carFleet.Count == 0 || carFleet.Peek() < time) {
+                carFleet.Push(time);
+            }
+        }
+        return carFleet.Count;
+    }
+}
